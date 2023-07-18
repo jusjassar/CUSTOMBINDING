1a6eed89107cf09d7c26f6b1cc8847eb1f3d95c2
Jus Jassar <jusjassar@gmail.com>
2023-07-18 11:27:47 -0400

    logic in component class

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -10,9 +10,17 @@ export class FilterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+this.radioSelectionToParent.emit(this.coursesSelected)
   }
-@Input('total') all:number=19;
-@Input() free:number=9;
-@Input() premium:number=10;
+  @Input('total') all: number = 19;
+  @Input() free: number = 9;
+  @Input() premium: number = 10;
+  coursesSelected: string = 'All';
+  @Output() radioSelectionToParent: EventEmitter<string> = new EventEmitter;
+
+  radioButtonSelected(radioSelection: string) {
+    console.log(radioSelection)
+    this.radioSelectionToParent.emit(radioSelection)
+  }
 }
 
